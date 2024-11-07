5042a2d7c3fad1eec6423815b342fb12d61600ce
brooke-branson <100882950+brooke-branson@users.noreply.github.com>
2024-11-07 12:30:28 -0800

    Crated print window class to help with large prints, such as box scores. Allows for the entire box score to be seen.
    Implemented the print window into the Teams object for use. Added a plotting option for the HR leaders to make a visual representation possible.
    
    Added last_game funciton to show the box score for the most recent regular season game played by the team.

--- team.py
+++ team.py
@@ -1,5 +1,9 @@
 import statsapi
 import pandas as pd
+import matplotlib
+matplotlib.use('TkAgg')
+from print_window import PrintWindow
+import matplotlib.pyplot as plt
 
 class Team:
     """
@@ -18,6 +22,8 @@ class Team:
         # self.nickname:
         self.id = id
         self.city = city
+
+        self.printer = PrintWindow()
 
     def help(self, type=None):
         """
@@ -41,11 +47,12 @@ class Team:
         else:
             statsapi.meta(type=type)
 
-    def hr_leaders(self, limit=10):
+    def hr_leaders(self, limit=10, plot=False):
         """
         Prints a list of the HR Leaders based on the amount passed thru. Default is top 10
 
         :param limit: Default value = 10. How deep you want the list to be
+                plot: Boolean. If set to True, will show a plot of the HR leaders, Defualt = False.
 
         :return: Returns a usable Pandas DataFrame called hr_df
         """
@@ -53,9 +60,39 @@ class Team:
         hr_info = statsapi.team_leaders(teamId=self.id, leaderCategories="homeRuns", limit=limit)
 
         usable = statsapi.team_leader_data(teamId=self.id, leaderCategories='homeRuns', limit=limit)
-
+        names = []
+        values = []
+        for x in usable:
+            names.append(x[1])
+            values.append(int(x[2]))
+            
         hr_df = pd.DataFrame(usable, columns=['Rank', 'Name', 'Value'])
+
+        if plot:
+            plt.figure(figsize=(10, 5))  # Adjust figure size if needed
+            plt.plot(names, values, marker='o')
+
+            # Add text annotations for each point
+            for i, (player, value) in enumerate(zip(names, values)):
+                plt.text(i, value, player, ha='right', va='bottom', rotation=0, fontsize=11)
+
+            plt.xlabel("Players")
+            plt.ylabel("HRs")
+            plt.xticks([])
+            plt.yticks(list(range(0, max(values))))
+            plt.title(f'{self.name} HR Leaders')
+            plt.show()
 
         print(hr_info)
 
         return hr_df
+
+    def last_game(self):
+        """
+        Prints the info for the last game played by the team.
+
+        :return:
+        """
+        self.printer.print(statsapi.boxscore(gamePk=statsapi.last_game(self.id)))
+        self.printer.window.mainloop()
+
